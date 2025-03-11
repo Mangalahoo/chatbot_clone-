e386dbd9779689947792251cc55cd1d273060228
Mangalahoo <parmesh.v@somaiya.edu>
2025-03-11 18:20:53 +0530

    Websocket done

--- server/venv/main.py
+++ server/venv/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+import asyncio
+from fastapi import FastAPI, WebSocket
 
 from pydantic_models.chat_body import ChatBody
 from services.llm_service import LLMService
@@ -10,6 +11,41 @@ app = FastAPI()
 search_service = SearchService()
 sort_source_services = SortSourceServices()
 llm_service = LLMService()
+
+
+#chat websocket
+@app.websocket("/ws/chat")
+async def websocket_chat_endpoint (websocket: WebSocket):
+    await websocket.accept()
+
+    try : 
+        await asyncio.sleep(0.1)
+
+        data = await websocket.receive_json()
+        query = data.get("query")
+
+        search_results = search_service.web_search(query)
+        sorted_results = sort_source_services.sort_source(query,search_results)
+        await websocket.send_json({
+            "type":"search_results",
+            "data":sorted_results
+        })
+        
+        
+        
+        for chunk in  llm_service.generate_response(query,sorted_results):
+            await asyncio.sleep(0.1)
+            await websocket.send_json({
+                "type":"content",
+                "data":chunk
+            })
+        
+  
+    except :
+        print("unexpected error occured")
+    finally:
+            await websocket.close()
+
 #chat 
 @app.post("/chat")
 def chat_endpoint(body :ChatBody):
